1b8758f1c964caf7f5873a0aa66041b132176c55
Serena Chandler <serenachandler2019@gmail.com>
2021-05-24 11:42:32 -0700

    created card component

---
+++ src/components/recipeCards/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
+const Recipes = () => {
+  return (<div>
+
+
+
+{Recipe.map((recipe) => (
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={recipe.image} />
+  <Card.Body>
+    <Card.Title>{recipe.name}</Card.Title>
+    <Card.Text>
+     {recipe.}
+    </Card.Text>
+    <Button variant="primary">{recipe.link}</Button>
+  </Card.Body>
+</Card>
+))}
+
+
+
+
+
+
+
+  </div>
+  
+  )
+};
