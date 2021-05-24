64a9d014c471c7a368fb7bc1d0e874dd2aa0e325
Serena Chandler <serenachandler2019@gmail.com>
2021-05-24 11:44:25 -0700

    formatted card component

--- src/components/recipeCards/index.js
+++ src/components/recipeCards/index.js
@@ -3,32 +3,20 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 
 const Recipes = () => {
-  return (<div>
-
-
-
-{Recipe.map((recipe) => (
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src={recipe.image} />
-  <Card.Body>
-    <Card.Title>{recipe.name}</Card.Title>
-    <Card.Text>
-     {recipe.description}
-    </Card.Text>
-    <Button variant="primary">{recipe.link}</Button>
-  </Card.Body>
-</Card>
-))}
-
-
-
-
-
-
-
-  </div>
-  
-  )
+  return (
+    <div>
+      {Recipe.map((recipe) => (
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={recipe.image} />
+          <Card.Body>
+            <Card.Title>{recipe.name}</Card.Title>
+            <Card.Text>{recipe.description}</Card.Text>
+            <Button variant="primary">{recipe.link}</Button>
+          </Card.Body>
+        </Card>
+      ))}
+    </div>
+  );
 };
 
-export default Recipes
+export default Recipes;
