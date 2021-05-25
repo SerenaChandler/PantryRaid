2c4da11c2d0c0dfc452702a6e1f7e947023cf5b9
Thomas Peeler <Thomasmwpeeler@Gmail.com>
2021-05-25 10:51:49 -0700

    add hero, navbar, recipeheader, and search bar components

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg">
+            <ul>
+                <a href="/home">LOGO HERE</a>
+                <button className="btn btn-outline-primary mx-4">Pantry</button>
+                <button className="btn btn-outline-primary mx-4">Cookbook</button>
+                <button className="btn btn-outline-primary mx-4">Sign In / Register</button>
+            </ul>
+        </nav>
+
+
+
+
+    )
+
+}
+
+export default Navbar;
