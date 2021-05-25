deb72cad6da62676de585269ccf953cb167c21f1
Thomas Peeler <Thomasmwpeeler@Gmail.com>
2021-05-25 11:13:02 -0700

    add routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 // imports
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home"
+import Cookbook from "./pages/Cookbook"
+import Pantry from "./pages/Pantry"
+import notFound from "./pages/notFound"
+
 import Navbar from "./components/Navbar"
 import Searchbar from "./components/Searchbar"
 import RecipeHeader from "./components/RecipeHeader"
@@ -8,7 +13,14 @@ import ChecklistDropdown from "./components/ChecklistDropdown"
 
 function App() {
     return(
-        
+        <Router>
+            <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/cookbook" component={Cookbook}/>
+            <Route exact path="/pantry" component={Pantry}/>
+            <Route component={notFound}/>
+            </Switch>
+        </Router>
 
 
 
