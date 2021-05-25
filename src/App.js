749636a92005e97c392dde0749280a52d6ffc301
Thomas Peeler <Thomasmwpeeler@Gmail.com>
2021-05-25 11:08:54 -0700

    add pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 // imports
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar"
+import Searchbar from "./components/Searchbar"
+import RecipeHeader from "./components/RecipeHeader"
+import ChecklistDropdown from "./components/ChecklistDropdown"
 
 function App() {
     return(
-
+        
 
 
 
