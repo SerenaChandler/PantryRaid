b38205c2b693c684a64597335f373c1a73b23b2f
Serena Chandler <serenachandler2019@gmail.com>
2021-05-25 10:56:12 -0700

    made controllers

--- controllers/ingredientController.js
+++ controllers/ingredientController.js
@@ -12,13 +12,8 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  create: function(req, res) {
+  save: function(req, res) {
     db.Ingredient.create(req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  update: function(req, res) {
-    db.Ingredient.findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
