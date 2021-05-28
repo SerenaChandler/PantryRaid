049d826ee0166d9f46f1a1e85252f2023d1fb7e7
Thomas Peeler <Thomasmwpeeler@Gmail.com>
2021-05-28 11:22:04 -0700

    change name of cookbook export

--- src/pages/Cookbook.js
+++ src/pages/Cookbook.js
@@ -4,24 +4,26 @@ import IngredientContainer from "../components/IngredientContainer";
 import CookbookRecipes from "../components/CookbookRecipes"
 // import ChecklistDropdown from "./components/ChecklistDropdown"
 
-const Pantry = () => {
+const Cookbook = () => {
     // functions for handling search and checkboxes
     return (
+
         <div className="container-fluid">
             <div className="row">
                 <div className="col-lg-12 min-vh-100">
                     <div className="row">
                         <div className="d-flex justify-content-center">
                             <CookbookRecipes />
-                            {/* maybe this needs to be passed a function or prop or something for keeping track of the missing ingredients
-                        </div> */}
+                            {/* maybe this needs to be passed a function or prop or something for keeping track of the missing ingredients */}
+                        
                     </div>
                 </div>
             </div>
+        </div>
         </div>
 
 
     );
 };
 
-export default Pantry;
+export default Cookbook;
