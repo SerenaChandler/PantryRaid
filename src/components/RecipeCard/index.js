03a5d89ef3c249dc52453fe7274716eaedc9b6eb
Thomas Peeler <Thomasmwpeeler@Gmail.com>
2021-05-28 13:39:00 -0700

    add light text to cards

--- src/components/RecipeCard/index.js
+++ src/components/RecipeCard/index.js
@@ -6,20 +6,20 @@ const RecipeCard = (props) => {
   const { title, image, description, ingredients, nutrition } = props
   return (
     <div>
-<div className="recipecard">
-<div className="left" style={{ backgroundImage: `url("${image}")`}}>&nbsp;</div>
-<div className="right"><h3>{title}</h3>
-<p className="description">{description}</p>
-<div className="ingredients">
-<ul style={{"list-style": "none"}}>
-      {ingredients.map((ingredient) => <li>{ingredient}</li>)}
-        
-      
-    </ul>
-</div>
+      <div className="recipecard">
+        <div className="left" style={{ backgroundImage: `url("${image}")` }}>&nbsp;</div>
+        <div className="right"><h3 className="text-light">{title}</h3>
+          <p className="description text-light">{description}</p>
+          <div className="ingredients">
+            <ul style={{ "list-style": "none" }}>
+              {ingredients.map((ingredient) => <li className="text-light">{ingredient}</li>)}
 
-</div>
-</div>
+
+            </ul>
+          </div>
+
+        </div>
+      </div>
 
     </div>
   );
