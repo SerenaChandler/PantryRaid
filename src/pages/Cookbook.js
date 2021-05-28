238b7e19ad0d5643a1a8b0dce8ca69109b7470fd
Thomas Peeler <Thomasmwpeeler@Gmail.com>
2021-05-28 11:33:28 -0700

    get rid of unnecessary imports

--- src/pages/Cookbook.js
+++ src/pages/Cookbook.js
@@ -1,13 +1,10 @@
 import React, { useEffect } from "react";
-import PantryMyFridge from "../components/PantryMyFridge";
-import IngredientContainer from "../components/IngredientContainer";
 import CookbookRecipes from "../components/CookbookRecipes"
 // import ChecklistDropdown from "./components/ChecklistDropdown"
 
 const Cookbook = () => {
     // functions for handling search and checkboxes
     return (
-
         <div className="container-fluid">
             <div className="row">
                 <div className="col-lg-12 min-vh-100">
@@ -15,11 +12,11 @@ const Cookbook = () => {
                         <div className="d-flex justify-content-center">
                             <CookbookRecipes />
                             {/* maybe this needs to be passed a function or prop or something for keeping track of the missing ingredients */}
-                        
+
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         </div>
 
 
