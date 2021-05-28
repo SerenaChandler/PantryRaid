85a777360a10e6acc02707981c368275f6a2d85e
Patrick Ly <patrickly72@gmail.com>
2021-05-28 12:48:46 -0700

    added styling for recipe cards

--- src/components/RecipeCard/index.js
+++ src/components/RecipeCard/index.js
@@ -1,53 +1,25 @@
 import React from "react";
-import Recipes from "../../recipe.json";
+import "./recipecard.css"
 
 
 const RecipeCard = (props) => {
   const {title, image, description, ingredients, nutrition} = props
   return (
     <div>
+<div className="recipecard">
+<div className="left" style={{ backgroundImage: `url("${image}")`}}>&nbsp;</div>
+<div className="right"><h3>{title}</h3>
+<p className="description">{description}</p>
+<div className="ingredients">
+<ul style={{"list-style": "none"}}>
+      {ingredients.map((ingredient) => <li>{ingredient}</li>)}
+        
       
+    </ul>
+</div>
 
-     
-{/* 
-        // <Card style={{ width: "18rem" }} className="mt-5 py-5">
-        //   <Card.Img variant="top" src={recipe.image}/>
-        //   <Card.Body>
-        //     <Card.Title>{recipe.name}</Card.Title>
-        //     <Card.Text>{recipe.description}</Card.Text>
-        //     <Button variant="primary">{recipe.link}</Button>
-        //   </Card.Body>
-        // </Card>
-         */}
-        <div class="row mt-4">
-                            <div class="col-3 text-center">
-                                <h1>{title}</h1>
-                                <img src={image} alt="" width="250" height="250"/>
-                            </div>
-
-                            <div class="col-3 text-center">
-                                <h1>Description</h1>
-                                <p>{description}</p>
-                            </div>
-
-                            <div class="col-3 text-center">
-                                <h1>Ingredients</h1>
-                                <ul style={{"list-style": "none"}}>
-                                  {ingredients.map((ingredient) => <li>{ingredient}</li>)}
-                                    
-                                  
-                                </ul>
-                            </div>
-
-                            <div class="col-3 text-center">
-                                <h1>Nutritional info</h1>
-                                <p>{nutrition}</p>
-
-                            </div>
-                        </div>
-
-
-      
+</div>
+</div>
     </div>
   );
 };
