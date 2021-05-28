8ea30acc1f79a21279220e1ea28118d28a6ec0c2
Thomas Peeler <Thomasmwpeeler@Gmail.com>
2021-05-28 12:32:55 -0700

    add logo to navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,14 +1,14 @@
 import React from "react";
-
+import logo from "../Navbar/pantryraid.png"
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg">
             <ul>
-                <a href="/home">LOGO HERE</a>
-                <button className="btn btn-outline-primary mx-4"><a href="/pantry" alt="pantry button" className="text-decoration-none">Pantry</a></button>
-                <button className="btn btn-outline-primary mx-4"><a href="/cookbook" alt="pantry button"className="text-decoration-none">Cookbook</a></button>
-                <button className="btn btn-outline-primary mx-4"><a href="/login" alt="pantry button"className="text-decoration-none">Sign In/ Register</a></button>
+                <a href="/home"><img src={logo} alt="chef flag with skull and crossbones as a knife and spatula" width="100"/></a>
+                <a href="/pantry" alt="pantry button" className="text-decoration-none"><button className="btn btn-outline-primary mx-4">Pantry</button></a>
+                <a href="/cookbook" alt="pantry button"className="text-decoration-none"><button className="btn btn-outline-primary mx-4">Cookbook</button></a>
+                <a href="/login" alt="pantry button"className="text-decoration-none"><button className="btn btn-outline-primary mx-4">Sign In/ Register</button></a>
             </ul>
         </nav>
 
