7f3ff79b389d568b2fa798308f850ae653d78e40
Patrick Ly <patrickly72@gmail.com>
2021-06-03 11:36:50 -0700

    made hero mobile responsive

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./style.css"
 
 
 const Hero = () => {
@@ -7,7 +8,7 @@ const Hero = () => {
                     <div className="col-lg-12">
                         <div className="hero text-center py-5 d-flex justify-content-center">
                             {/* <h1 className="display-5 text-light py-5">Pantry Raid</h1> */}
-                            <img alt="colorful fridge magnet Pantry raid text" src="pantryraidlogo3.png" />
+                            <img className="responsive" alt="colorful fridge magnet Pantry raid text" src="pantryraidlogo3.png" />
                         </div>
                     </div>
                 </div>
