57b5f811e63b77780f20d602163c669cd7fa0a9e
Thomas Peeler <Thomasmwpeeler@Gmail.com>
2021-06-03 15:55:21 -0700

    add logo to broken images

--- client/src/components/ingredientCards/index.js
+++ client/src/components/ingredientCards/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 import "./style.css";
-
+import logo from "../../logo.png"
 const Ingredients = (props) => {
   const { label, image, nutrition, saveIngredient } = props;
 
   return (
     <li className="list-group-item list-group-item-dark">
-      <img src={image} width="50px" height="50px" />
+      <img src={image? image: logo} width="50px" height="50px" />
       <p className="label">
         {" "}
         {label} <button onClick={saveIngredient}>Save to Fridge</button>
