ec854cbb04f6637bfea4042b718c8f0a8b6de356
markosanchez800 <markosanchez800@gmail.com>
2021-06-04 12:34:22 -0700

    new idea for ingredient cards

--- client/src/components/ingredientCards/index.js
+++ client/src/components/ingredientCards/index.js
@@ -6,7 +6,7 @@ const Ingredients = (props) => {
   const { label, image, nutrition, saveIngredient } = props;
 
   return (
-    <li className="list-group-item list-group-item-dark">
+    <li className="list-group-item">
       <img src={image? image: logo} width="50px" height="50px" />
       <p className="label">
         {" "}
@@ -19,3 +19,25 @@ const Ingredients = (props) => {
 export default Ingredients;
 
 // comment
+
+// import React from "react";
+// import ListGroup from "react-bootstrap/ListGroup";
+// import "./style.css";
+// import logo from "../../logo.png"
+// const Ingredients = (props) => {
+//   const { label, image, nutrition, saveIngredient } = props;
+
+//   return (
+//     <li className="list-group-item list-group-item-dark">
+//       <img src={image? image: logo} width="50px" height="50px" />
+//       <p className="label">
+//         {" "}
+//         {label} <button onClick={saveIngredient}>Save to Fridge</button>
+//       </p>
+//     </li>
+//   );
+// };
+
+// export default Ingredients;
+
+// // comment
